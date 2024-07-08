a65cdc04514aa3b20d73e907350f3435aa11084a
dabrickashaw60 <121248625+dabrickashaw60@users.noreply.github.com>
2024-07-08 15:21:38 -0400

    submitting user login project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!  
+
+  private
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || authenticated_root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    unauthenticated_root_path
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
+  get 'home/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  authenticated :user do
+    root 'posts#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'posts#index', as: :unauthenticated_root
+  end
+
+  resources :posts, only: [:new, :create, :index]
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
